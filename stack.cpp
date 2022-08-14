9ace21f93cafb28869f2c5598d0a07fe31795629
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-08-14 15:13:06 +0200

    implement a stack in C++

---
+++ stack.cpp
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <iostream>
+
+class Vector2 {
+public:
+  float x, y;
+};
+
+class Entity {
+public:
+  Vector2 position;
+  float rotation;
+  Vector2 scale;
+
+  Entity() {
+    this->position = {0, 0};
+    this->rotation = 0;
+    this->scale = {1, 1};
+  }
+  Entity(Vector2 position, float rotation, Vector2 scale) {
+    this->position = position;
+    this->rotation = rotation;
+    this->scale = scale;
+  }
+};
+
+template <class T> class Stack {
+private:
+  unsigned int capacity;
+  unsigned int size;
+  T *elements;
+
+public:
+  Stack(unsigned int capacity) {
+    this->capacity = capacity;
+    this->size = 0;
+    this->elements = new T[capacity];
+  }
+
+  ~Stack() { delete[] elements; }
+
+  inline bool empty() const { return size == 0; }
+
+  inline T *pop_back() {
+    if (size == 0)
+      return nullptr;
+
+    return elements[size - 1];
+    size--;
+  }
+
+  inline void push_back(T value) {
+    if (size == capacity) {
+      return;
+    }
+    elements[size] = value;
+    size++;
+  }
+
+  inline T *back() {
+    if (size == 0) {
+      return nullptr;
+    }
+    return &elements[size - 1];
+  }
+
+  inline T *front() {
+    if (size == 0) {
+      return nullptr;
+    }
+    return &elements[0];
+  }
+
+  inline T *get(unsigned int index) {
+    if (index < 0 || index >= size) {
+      return nullptr;
+    }
+    return &elements[index];
+  }
+
+  int get_capacity() const { return capacity; }
+};
+
+int main(int argc, char *argv[]) {
+  const int size = 20;
+  auto *stack = new Stack<Entity>(size);
+
+  for (int i = 0; i < stack->get_capacity(); i++) {
+    const Entity *entity = new Entity({10 + float(i), 10 + float(i)}, 5, {10, 10});
+    stack->push_back(*entity);
+  }
+
+  Entity *entity = stack->front();
+  std::cout << "Position: " << entity->position.x << ", " << entity->position.y << std::endl;
+  std::cout << "Rotation: " << entity->rotation << std::endl;
+  std::cout << "Scale: " << entity->scale.x << ", " << entity->scale.y << std::endl;
+
+  delete stack;
+
+  return 0;
+}
